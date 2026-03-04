f504dd3f70f50fd34ed60d24d3fc501cbb81ec85
hardik211005 <hardikdhameeja2105@gmail.com>
2026-03-04 22:30:52 +0530

    feat: implement project fetching and loading state in Projects page

--- client/src/pages/Projects.tsx
+++ client/src/pages/Projects.tsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import React, { use, useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { Loader2Icon } from 'lucide-react'
+import { dummyConversations, dummyProjects } from '../assets/assets'
+import type { Project } from '../types'
 
 const Projects = () => {
-  return (
+  const {projectId} = useParams()
+  const navigate = useNavigate()
+
+
+  const [project, setProject] = useState<Project | null >(null)
+  const [loading, setLoading] = useState(true)
+
+  const [isGenerating, setIsGenerating] = useState(true)
+  const [device, setDevice] = useState<'phone' | 'tablet' | 'desktop'>('desktop')
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [isSaving, setIsSaving] = useState(false)
+  const fetchProject = async () => {
+    const project = dummyProjects.find(project => project.id === projectId)
+    setTimeout(() => {
+      if(project){
+        setProject({...project, conversation: dummyConversations});
+        setLoading(false)
+        setIsGenerating(project.current_code ? false : true)
+      }
+      
+    },2000)
+}
+  useEffect(() => {
+    fetchProject()
+  },[])
+
+  if(loading){
+    return (
+      <>
+      <div className='flex items-center justify-center h-screen'>
+        <Loader2Icon className='size-7 animate-spin text-violet-200'/>
+      </div>
+      </>
+    )
+  }
+
+  return project ? (
     <div>Projects</div>
+  )
+  :
+  (
+    <div className='flex items-center justify-center h-screen'>
+      <p className='text-2xl font-medium text-gray-200'>Unable to load project!</p>
+    </div>
+  
   )
 }
 
