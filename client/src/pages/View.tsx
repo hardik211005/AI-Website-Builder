dbfbc2bb31cbc344fc7d15ef0d9337903cad5059
hardik211005 <hardikdhameeja2105@gmail.com>
2026-04-04 21:07:30 +0530

    feat: integrate Stripe for payment processing; implement webhook for payment confirmation and user credit updates

--- client/src/pages/View.tsx
+++ client/src/pages/View.tsx
@@ -4,6 +4,8 @@ import { dummyProjects } from "../assets/assets";
 import { Loader2Icon } from "lucide-react";
 import ProjectPreview from "../components/ProjectPreview";
 import type { Project } from "../types";
+import api from "@/configs/axios";
+import { toast } from "sonner";
 
 
 const View = () => {
@@ -12,13 +14,15 @@ const View = () => {
   const [loading, setLoading] = useState(true)
 
   const fetchCode = async () => {
-    const code = dummyProjects.find(project=> project.id === projectId)?.current_code;
-    setTimeout(()=> {
-      if(code){
-        setCode(code);
-        setLoading(false)
-      }
-    },2000)
+    try {
+      const {data} = await api.get(`/api/project/published/${projectId}`);
+      setCode(data.code)
+      setLoading(false);
+
+    } catch (error : any) {
+      toast.error(error?.response?.data?.message || error.message || 'Failed to fetch project');
+      console.error(error);
+    }
   }
   useEffect(()=> {
     fetchCode()
--- server/server.ts
+++ server/server.ts
@@ -6,6 +6,7 @@ import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth.js";
 import userRouter from "./routes/userroutes.js";
 import projectRouter from "./routes/projectRoutes.js";
+import { stripeWebhook } from "./controllers/stripeWebhook.js";
 
 
 const app = express();
@@ -15,6 +16,7 @@ const corsOptions = {
     credentials: true,
 }
 app.use(cors(corsOptions));
+app.post('/api/stripe', express.raw({type: 'application/json'}), stripeWebhook)
 app.all('/api/auth/{*any}', toNodeHandler(auth));
 
 app.use(express.json({limit: '50mb'}));
